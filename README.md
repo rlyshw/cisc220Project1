a80830fd9d42095a51f9c0f2677fe71381456cf7
Riley Shaw <rlyshw@gmail.com>
2015-09-22 00:55:07 +0000

    added p1.pp

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cisc220Project1
 Sam, Josh, Riley, Tom Brady
+
+Lets Do This Shiz Nit.
---
+++ p1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+/*Decide how to get data into an instance of your class.
+    *Overload +, *, - to combine two bigInts into another bigInt. (Or make a different choice and defend it.)
+    *Allow division of bigInts which returns a double, and a different division which returns the integer quotient as a bigInt.
+    *Overload << so that cout displays the bigInt well. Do >> for cin to.
+    *Give an indication of your performance at various sizes.
+    */
+    
+class bigInt {
+        /* We could use the power of tens and vectors to get the values 
+        *  so if the user input 321 it would be 1*1 2*10 3*100
+        */
+        
+        // vector<dataType> identifier(numElements);
+        //vector<bigInt> input(numElements);
+}
