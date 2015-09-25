cf6e8fb7650b738e1c388663f1e58d593cda729a
rlyshw <rlyshw@udel.edu>
2015-09-24 23:43:47 -0400

    cin works, overloaded constructor for char arrays)

--- BigInt.h
+++ BigInt.h
@@ -1,15 +1,17 @@
 #include<iostream>
-#include<sstream>
 using namespace std;
 
 class BigInt {
 public:
 	int val;
 	int* arr;
-	BigInt(int i);
+	BigInt(int);
+	BigInt(const char*);
 	BigInt operator+(const BigInt&);
 	BigInt operator*(const BigInt&);
 	BigInt operator-(const BigInt&);
 	BigInt operator/(const BigInt&);
+	friend istream& operator>>(istream &in, BigInt&);
 };
 ostream& operator<<(ostream &out, const BigInt&);
+//istream& operator>>(istream $in, const BigInt&);
--- main.cpp
+++ main.cpp
@@ -8,17 +8,11 @@ using namespace std;
 //}
 
 int main(){
-	//BigInt big = 3;
-	//BigInt l = 10;
-	//BigInt i = 20;
-	BigInt two = 1234567891;
-//	cout << two*two << endl;
-	BigInt a = 20;
-	cout << a*3 << endl;
-	//cout << four << endl;
-	//cout << (l+i) << endl;
-	//cout << (l*i) << endl;
-	//cout << (l-i) << endl;
-	//cout << (i/l)  << endl;
-	//cout << big << endl;
+	BigInt b = "123";
+	BigInt c = "321";
+	cout << b << " " << c << endl;
+	cout << b+c << endl;
+	BigInt d = 0;
+	cin >> d;
+	cout << d;
 }
