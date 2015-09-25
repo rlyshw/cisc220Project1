b84cb65850f14b0bb7b5e89d9226566f04c36837
Riley Shaw <rlyshw@gmail.com>
2015-09-25 18:31:37 +0000

    division in main

--- main.cpp
+++ main.cpp
@@ -2,17 +2,28 @@
 #include"BigInt.h"
 using namespace std;
 
-
-//BigInt operator+(const BigInt& intA, const BigInt& intB){
-    //return new BigInt(100);
-//}
+/* Performance at various sizes:
+Our program works for any "BigInts" to be added together that are put into the system from cin but when
+directly constructing a BigInt it cannot be anything bigger than the maximum int's value. 
+Addition performs at all sizes, within the real time of .01 seconds found by using time ./a.out
+Subtraction performs at all sizes, within the same real time as addtion but you cannot subract a 
+Multiplication of BigInts is preformed accurately up to the 100 million place.
+Division of BigInts does not work as a quotinet cannot be computed by each number in its value.
+*/
 
 int main(){
-	BigInt b = "123";
-	BigInt c = "321";
-	cout << b << " " << c << endl;
-	cout << b+c << endl;
-	BigInt d = 0;
-	cin >> d;
-	cout << d;
+	//BigInt big = 3;
+	//BigInt l = 10;
+	//BigInt i = 20;
+	//BigInt two = 1234567891;
+	//cout << two << endl;13086791879324657913
+	BigInt a = 12345678;
+	BigInt b = 1000000;
+	cout << a/b<< endl;
+	//cout << four << endl;
+	//cout << (l+i) << endl;
+	//cout << (l*i) << endl;
+	//cout << (l-i) << endl;
+	//cout << (i/l)  << endl;
+	//cout << big << endl;
 }
